7fbd8bdce4332c11a4c8aa1ef7bb9206a402d60d
Azalurg <91213389+Azalurg@users.noreply.github.com>
2023-11-27 11:55:32 +0100

    Add new stats to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,12 @@ class DashboardController < ApplicationController
     @clases = Hero.group(:class_name).order('count_all DESC').count()
     @races = Hero.group(:race).order('count_all DESC').count()
     @backgrounds = Hero.group(:background).order('count_all DESC').count()
-  end
+  
+    @avg_level = Hero.average(:level).to_f.round(2)
+    @median_level = Hero.median(:level)
+    @dominant_level = Hero.dominant(:level)
+    @min_level = Hero.minimum(:level)
+    @max_level = Hero.maximum(:level)
+    @total_heroes = Hero.count()
+end
 end
--- app/models/hero.rb
+++ app/models/hero.rb
@@ -4,22 +4,41 @@ class Hero < ApplicationRecord
   belongs_to :background
   has_one :user
 
+  before_save :level_up
+
   def get_exp_limit
-    if level <= 70
-      2 * level * (level - 1)
-    elsif level <= 150
-      5 * level * (level - 1) - 350 * level + 9941
-    elsif level > 150
-      9 * level * (level - 1) - 950 * level + 9940
+    if self.level <= 70
+      2 * self.level * (self.level - 1)
+    elsif self.level <= 150
+      5 * self.level * (self.level - 1) - 350 * self.level + 9941
+    elsif self.level <= 300
+      9 * self.level * (self.level - 1) - 950 * self.level + 9940
+    elsif self.level > 300
+      13 * self.level * (self.level - 1) - 2150 * self.level + 9935
     end
   end
 
   def level_up
-    while exp >= get_exp_limit
-      exp -= get_exp_limit
-      level += 1
-      free_points += 5 if level % 10 == 0
-      free_points += 5 
-    end   
+    while self.exp >= self.get_exp_limit
+      self.exp -= self.get_exp_limit
+      self.level += 1
+      self.free_points += 5 if self.level % 10 == 0
+      self.free_points += 5 
+    end
+  
   end
+
+  def self.median(column)
+    values = pluck(column).sort
+    len = values.length
+    (values[(len - 1) / 2] + values[len / 2]) / 2.0
+  end
+
+  def self.dominant(column)
+    rounded_values = pluck(column).map { |value| (value / 10.0).round * 10 }
+    counts = Hash.new(0)
+    rounded_values.each { |value| counts[value] += 1 }
+    dominant_values = counts.select { |_value, count| count == counts.values.max }.keys
+    dominant_values[0]
+    end
 end
